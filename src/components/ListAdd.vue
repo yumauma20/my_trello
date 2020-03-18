7c395fb9dc93a157ea4e2dd644879b4309596d45
yumauma20 <71yuma10@gmail.com>
2020-03-18 23:48:13 +0900

    フォーム部分の動的化

--- src/components/ListAdd.vue
+++ src/components/ListAdd.vue
@@ -1,7 +1,7 @@
 <template>
-    <form class="addlist" @submit.prevent="addList">
-        <input v-model="title" type="text" class="text-input" placeholder="Add new list">
-        <button type="submit" class="add-button">
+    <form :class="classList" @submit.prevent="addList">
+        <input v-model="title" type="text" class="text-input" placeholder="Add new list" @focusin="startEditing" @focusout="finishEditing">
+        <button type="submit" class="add-button" v-if="isEditing || titleExists">
             Add
         </button>
     </form>
@@ -12,12 +12,35 @@ export default {
     data: function() {
         return {
             title: '',
+            isEditing: false,
         }
+    },
+    computed: {
+        classList() {
+            const classList = ['addlist']
+            if (this.isEditing) {
+                classList.push('active')
+            }
+            if (this.titleExists) {
+                classList.push('addable')
+            }
+            return classList
+        },
+
+        titleExists() {
+            return this.title.length > 0
+        },
     },
     methods: {
         addList: function() {
             this.$store.dispatch('addlist', { title: this.title })
             this.title = ''
+        },
+        startEditing() {
+            this.isEditing = true
+        },
+        finishEditing() {
+            this.isEditing = false
         },
     }
 }
